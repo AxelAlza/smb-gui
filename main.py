120d7ba613107c5ef432e887da4a2b27303439cc
AxelAlza <axelalza1998@gmail.com>
2022-06-01 16:09:51 -0300

    filtro

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ from vistas.VentanaPrincipal import VentanaPrincipal
 
 if __name__ == "__main__":
     elevate()
-    os.system('xhost +si:localuser:root')
-    os.environ['DISPLAY'] = ":0"
+
     os.environ['XAUTHORITY'] = '~/.Xauthority'
+    os.environ['DISPLAY'] = ":0.0"
     app = QtWidgets.QApplication(sys.argv)
     v = VentanaPrincipal()
     v.show()
